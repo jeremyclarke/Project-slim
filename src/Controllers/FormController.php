08f9a46b3cd921accdb738a81b6c27946bf38a88
Jeremy <jeremyclarke100@gmail.com>
2017-03-01 22:10:46 +0000

    More to insert function

--- src/Controllers/FormController.php
+++ src/Controllers/FormController.php
@@ -17,16 +17,28 @@ class FormController
         $stmt = $this->dbconn->prepare($sql);
         $stmt->execute();
 
-        $results = $stmt->fetchAll(\PDO::FETCH_UNIQUE|\PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(\PDO::FETCH_UNIQUE | \PDO::FETCH_ASSOC);
 
         return $results;
     }
 
-    function submitForm($params) {
-        print_r($params);
+    function submitForm($params)
+    {
+        // print_r($params);
 
-        $originalSQL = "INSERT INTO project.testinput ( one, two, three ) VALUES (@@dropdown_1, @@dropdown_2, @@textbox_3')";
+        $originalSQL = "INSERT INTO project.testinput ( one, two, three ) VALUES (@@dropdown_1, @@dropdown_2, @@textbox_3)";
+        $sqlExplode = explode("@@", $originalSQL);
+        array_shift($sqlExplode);
 
-        $newSQL = str_replace()
+        print_r($sqlExplode);
+        echo '</br></br></br>';
+        // echo $originalSQL;
+
+        for ($i=0; i <= count($sqlExplode); $i++) {
+            //$newSQL = str_replace('@@' . $sqlExplode[i], $params[i], $originalSQL);
+            //echo $newSQL;
+        }
+
+        //$newSQL = str_replace();
     }
 }
